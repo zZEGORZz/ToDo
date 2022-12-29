9d970dc9e3ecee640069b688796b15d12a6ac6c5
zZEGORZz <somolot@bk.ru>
2022-12-29 16:09:58 +0300

    ToDo: experimental function

--- scripts/file.js
+++ scripts/file.js
@@ -1,5 +1,31 @@
+"use strict";
+
+
+let tasks = [
+    { task: 'go work' },
+    { task: 'go LearnJS' },
+    { task: 'go Learn' },
+    { task: 'go to work' }
+];
+
 buttonAddTask.onclick = function(){
     let inp = document.getElementById('inputTask').value;
+    //getToDoList(inp);
     document.getElementById('inputTaskText').innerHTML = inp;
     document.getElementById('inputTask').value = '';
 }
+
+function getToDoList(alltask){
+    let ul = document.querySelector('.ulTodo');
+    
+    let liElements = alltask.map((elem) => {
+        let li = document.createElement('li');
+        li.classList.add('liTodo');
+        li.append(elem.task);
+        return li;
+    })
+    return ul.append(...liElements);
+}
+
+getToDoList(tasks);
+
