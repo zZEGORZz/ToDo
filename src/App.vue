b45e49579cb5c6170fbc2cfe383b89ecc667920b
zZEGORZz <somolot@bk.ru>
2023-01-02 16:00:40 +0300

    Create spa project (without ui kit)

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <!-- eslint-disable -->
+  <div class="header">
+        <div class="container">
+            <div class="logo">Задачи</div>
+            <input 
+                id="inputTask" 
+                type="text" 
+                placeholder="Введите задачу" 
+                :value="inputValue" 
+                @input="inpValueNow"
+                @keypress.enter="addTask"
+            >
+            <input id="buttonAddTask" type="button" value="Добавить" @click="addTask">
+        </div>
+    </div>
+    <ul>
+        <li v-for="(elem, indexElem) in listTasks" :key="indexElem.id"> {{ elem }} 
+            <div>
+                <button class="btn btn2" @click="deleteTask(indexElem)">Удалить</button>
+            </div>
+        </li>
+        
+    </ul>
+</template>
+
+<script>
+export default {
+  data() {
+        return{
+            inputValue: '',
+            listTasks: []
+        }
+    },  
+  methods: {
+        addTask(){
+            if(this.inputValue !== ''){
+                this.listTasks.push(this.inputValue);
+                this.inputValue = '';
+            }
+        },
+        inpValueNow(event){
+            this.inputValue = event.target.value;
+        },
+        deleteTask(indexElem){
+            this.listTasks.splice(indexElem, 1);
+        }
+  }   
+}
+</script>
